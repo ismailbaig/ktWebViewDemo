972aa0cbc007b8e1be3caba9bb8ca33684537c39
ismail1986 <ismailece2007@gmail.com>
2020-05-16 21:46:55 +0530

    kotlin web view sample

---
+++ app/src/main/java/com/example/kotlinwebview/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.kotlinwebview
+
+import android.app.Activity
+import android.os.Build
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.webkit.WebResourceError
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //setSupportActionBar(toolbar)
+
+        val myWebView: WebView = this.findViewById(R.id.wvUrl);
+
+        myWebView.webViewClient = MyWebViewClient(this)
+        myWebView.loadUrl(("https://www.gmail.com/"))
+        myWebView.settings.javaScriptEnabled = true
+    }
+
+    class MyWebViewClient internal constructor(private  val activity: Activity) : WebViewClient(){
+        @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+        override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+            val url: String = request?.url.toString();
+            view?.loadUrl(url)
+            return true
+        }
+
+        override fun shouldOverrideUrlLoading(webView: WebView, url: String): Boolean {
+            webView.loadUrl(url)
+            return true
+        }
+
+        override fun onReceivedError(view: WebView, request: WebResourceRequest, error: WebResourceError) {
+            Toast.makeText(activity, "Got Error! $error", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
